054c888cb7f26cd910e7a9490c262b4993f2b1fb
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-03-05 20:37:44 +0700

    Jobsheet 2 - Praktikum 2(NO 6)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', [WelcomeController::class, 'hello']);
@@ -9,20 +10,14 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/about', function () {
-    return 'NIM - 2341720049 | Hikmah Aldrin Abdillah';
-});
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
-});
-
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
 });
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
